71f38c51fddcb68abb1504fccbb520f79d3308f5
anuragsingh2207 <anuragsingh2207@gmail.com>
2024-07-17 11:27:37 +0530

    Updated python script

--- sql/db.sql
+++ sql/db.sql
@@ -33,7 +33,39 @@ CREATE TABLE Singer2 (
 CREATE TABLE Singer3 (
             SingerId     INT64 NOT NULL,
             FirstName    STRING(1024),
-            LastName     STRING(102),
+            LastName     STRING(1024),
+            SingerInfo   BYTES(MAX),
+            FullName   STRING(2048) AS (
+                ARRAY_TO_STRING([FirstName, LastName], " ")
+            ) STORED
+        ) PRIMARY KEY (SingerId)""",
+            """CREATE TABLE Albums (
+            SingerId     INT64 NOT NULL,
+            AlbumId      INT64 NOT NULL,
+            AlbumTitle   STRING(MAX)
+        ) PRIMARY KEY (SingerId, AlbumId),
+        INTERLEAVE IN PARENT Singers ON DELETE CASCADE;
+
+CREATE TABLE Singer4 (
+            SingerId     INT64 NOT NULL,
+            FirstName    STRING(1024),
+            LastName     STRING(1024),
+            SingerInfo   BYTES(MAX),
+            FullName   STRING(2048) AS (
+                ARRAY_TO_STRING([FirstName, LastName], " ")
+            ) STORED
+        ) PRIMARY KEY (SingerId)""",
+            """CREATE TABLE Albums (
+            SingerId     INT64 NOT NULL,
+            AlbumId      INT64 NOT NULL,
+            AlbumTitle   STRING(MAX)
+        ) PRIMARY KEY (SingerId, AlbumId),
+        INTERLEAVE IN PARENT Singers ON DELETE CASCADE;
+
+CREATE TABLE Singer5 (
+            SingerId     INT64 NOT NULL,
+            FirstName    STRING(1024),
+            LastName     STRING(1024),
             SingerInfo   BYTES(MAX),
             FullName   STRING(2048) AS (
                 ARRAY_TO_STRING([FirstName, LastName], " ")
