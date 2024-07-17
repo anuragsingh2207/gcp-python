e188a0e7b0bdc6ede12d0e88e0fa31e7a2e0cbb2
Anurag Kumar Singh <64636228+anuragsingh2207@users.noreply.github.com>
2024-07-17 23:32:54 +0530

    Testing

--- scripts/db.py
+++ scripts/db.py
@@ -1,7 +1,7 @@
-# import os
-# import subprocess
+import os
+import subprocess
 
-# print("Running python script..")
+print("Running python script..")
 
 # # Get diff between the latest commit and current uncommitted changes
 # result = subprocess.run(['git', 'diff', '--unified=0', 'HEAD', '../sql/db.sql'], stdout=subprocess.PIPE)
@@ -15,22 +15,9 @@
 #     print(line)
 
 
-import subprocess
-
-print("Running python script...")
-
-
-print("Print Current Directory..")
-result = subprocess.run(["pwd"], capture_output=True, text=True)
-print(result.stdout)
-
-print("Listing files in Current Directory..")
-result1 = subprocess.run(["pwd"], capture_output=True, text=True)
-print(result1.stdout)
-
 
 # Get diff between the HEAD and current state
-result = subprocess.run(['git', 'diff', '--unified=0', 'HEAD', '../sql/db.sql'], stdout=subprocess.PIPE)
+result = subprocess.run(['git', 'diff', '--unified=0', 'HEAD', './sql/db.sql'], stdout=subprocess.PIPE)
 diff_output = result.stdout.decode('utf-8')
 
 # Extract additions in the current uncommitted changes
