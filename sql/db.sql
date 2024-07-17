3220441bfbdc4954257be03a4777335652fdc0f8
anuragsingh2207 <anuragsingh2207@gmail.com>
2024-07-17 11:29:57 +0530

    Added new table for testing

--- sql/db.sql
+++ sql/db.sql
@@ -77,3 +77,19 @@ CREATE TABLE Singer5 (
             AlbumTitle   STRING(MAX)
         ) PRIMARY KEY (SingerId, AlbumId),
         INTERLEAVE IN PARENT Singers ON DELETE CASCADE;
+
+CREATE TABLE Singer6new (
+            SingerId     INT64 NOT NULL,
+            FirstName    STRING(1024),
+            LastName     STRING(1024),
+            SingerInfo   BYTES(MAX),
+            FullName   STRING(2048) AS (
+                ARRAY_TO_STRING([FirstName, LastName], " ")
+            ) STORED
+        ) PRIMARY KEY (SingerId)""",
+            """CREATE TABLE Albums (
+            SingerId     INT64 NOT NULL,
+            AlbumId      INT64 NOT NULL,
+            AlbumTitle   STRING(MAX)
+        ) PRIMARY KEY (SingerId, AlbumId),
+        INTERLEAVE IN PARENT Singers ON DELETE CASCADE;
