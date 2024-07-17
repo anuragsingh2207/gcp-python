e26b903e95d0162cb619c1e6346caabc2f5d9553
Anurag Kumar Singh <64636228+anuragsingh2207@users.noreply.github.com>
2024-07-17 23:36:06 +0530

    Updated

--- scripts/db.py
+++ scripts/db.py
@@ -3,22 +3,15 @@ import subprocess
 
 print("Running python script..")
 
-# # Get diff between the latest commit and current uncommitted changes
-# result = subprocess.run(['git', 'diff', '--unified=0', 'HEAD', '../sql/db.sql'], stdout=subprocess.PIPE)
-# diff_output = result.stdout.decode('utf-8')
 
-# # Extract additions in the current uncommitted changes
-# new_lines = [line[1:] for line in diff_output.splitlines() if line.startswith('+') and not line.startswith('+++')]
-
-# # Print the list of newly added lines
-# for line in new_lines:
-#     print(line)
-
-
-
-# Get diff between the HEAD and current state
-result = subprocess.run(['git', 'diff', '--unified=0', 'HEAD', './sql/db.sql'], stdout=subprocess.PIPE)
+# Change directory to the location of db.sql
+os.chdir('../sql/')
+  
+# Perform git diff
+result = subprocess.run(['git', 'diff', '--unified=0', 'HEAD', 'db.sql'], stdout=subprocess.PIPE)
 diff_output = result.stdout.decode('utf-8')
+
+print(diff_output)
 
 # Extract additions in the current uncommitted changes
 new_lines = [line[1:] for line in diff_output.splitlines() if line.startswith('+') and not line.startswith('+++')]
