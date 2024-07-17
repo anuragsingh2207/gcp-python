b337ca14fde8c451d5840ad155ffc9f3d046b874
Anurag Kumar Singh <64636228+anuragsingh2207@users.noreply.github.com>
2024-07-17 23:52:48 +0530

    Debugging

--- scripts/db.py
+++ scripts/db.py
@@ -13,15 +13,20 @@ print("Running python script..")
 # # print current working directory
 # print(os.getcwd())
 
+print("Listing files in current directory")
+result = subprocess.run(["ls", "-l"], capture_output=True, text=True)
+print(result.stdout)
+
+
 # Perform git diff
-result = subprocess.run(['git', 'diff', '--unified=0', 'HEAD', 'db.sql'], stdout=subprocess.PIPE)
-diff_output = result.stdout.decode('utf-8')
+# result = subprocess.run(['git', 'diff', '--unified=0', 'HEAD', 'db.sql'], stdout=subprocess.PIPE)
+# diff_output = result.stdout.decode('utf-8')
 
-print(diff_output)
+# print(diff_output)
 
-# Extract additions in the current uncommitted changes
-new_lines = [line[1:] for line in diff_output.splitlines() if line.startswith('+') and not line.startswith('+++')]
+# # Extract additions in the current uncommitted changes
+# new_lines = [line[1:] for line in diff_output.splitlines() if line.startswith('+') and not line.startswith('+++')]
 
-# Print the list of newly added lines
-for line in new_lines:
-    print(line)
+# # Print the list of newly added lines
+# for line in new_lines:
+#     print(line)
