ec1520ffd596e6ff5c99085e0962df5b7f0d0b17
Anurag Kumar Singh <64636228+anuragsingh2207@users.noreply.github.com>
2024-07-17 10:39:07 +0530

    Added another DDL for testing

--- scripts/db.py
+++ scripts/db.py
@@ -1,13 +1,24 @@
-import os
 
-# Read SQL file
+import os
+import subprocess
+
+# Get diff between the latest commit and current uncommitted changes
+result = subprocess.run(['git', 'diff', 'HEAD', '/sql/db.sql'], stdout=subprocess.PIPE)
+diff_output = result.stdout.decode('utf-8')
+
+# Extract additions in the current uncommitted changes
+new_commands = [line[1:] for line in diff_output.splitlines() if line.startswith('+')]
+
+# Read the old SQL file
 with open('/sql/db.sql', 'r') as f:
     sql_file = f.read()
 
 # Split the file into separate queries
-sql_commands = sql_file.split(';')
+old_commands = sql_file.split(';')
 
-for command in sql_commands:
-    # Let's print each command separately
-    print(command)
+# Compare and get a list of new queries
+added_queries = list(set(new_commands) - set(old_commands))
 
+# Print the list of newly added queries
+for query in added_queries:
+    print(query)
