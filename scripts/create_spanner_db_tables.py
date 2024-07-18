8f1b5309dd903b5333c97d5b197ec2ac825cad48
Anurag Kumar Singh <64636228+anuragsingh2207@users.noreply.github.com>
2024-07-18 12:01:30 +0530

    debugging

--- scripts/create_spanner_db_tables.py
+++ scripts/create_spanner_db_tables.py
@@ -14,88 +14,8 @@ from google.cloud.spanner_v1 import DirectedReadOptions, param_types
 from google.cloud.spanner_v1.data_types import JsonObject
 
 
-# OPERATION_TIMEOUT_SECONDS = 240
-# instance_id="demo-instance"
-# database_id="demo-database"
+GOOGLE_APPLICATION_CREDENTIALS=os.environ['GOOGLE_APPLICATION_CREDENTIALS']
 
-# print("Running python script...")
-
-# print("Current directory:")
-# result = subprocess.run(["pwd"], capture_output=True, text=True)
-# print(result.stdout)
-
-# print("Listing files in current directory")
-# result = subprocess.run(["ls", "-l"], capture_output=True, text=True)
-# print(result.stdout)
-
-# print("Changing directory to 'sql' and staying there")
-# os.chdir('../sql')
-
-# print("Current directory after change:")
-# result = subprocess.run(["pwd"], capture_output=True, text=True)
-# print(result.stdout)
-
-# print("Listing files in new current directory")
-# result = subprocess.run(["ls", "-l"], capture_output=True, text=True)
-# print(result.stdout)
-
-
-# # Get diff between the latest commit and the previous commit
-# print("Print newly added lines with special character...")
-# result = subprocess.run(['git', 'diff', '--unified=0', 'HEAD^', 'db.sql'], stdout=subprocess.PIPE)
-# diff_output = result.stdout.decode('utf-8')
-
-# # Print the diff output
-# print(diff_output)
-
-# print("Listing files in new current directory")
-# result = subprocess.run(["ls", "-l"], capture_output=True, text=True)
-# print(result.stdout)
-
-
-# print("Print newly added lines...")
-# # Extract additions in the current uncommitted changes
-# new_lines = [line[1:] for line in diff_output.splitlines() if line.startswith('+') and not line.startswith('+++')]
-
-# # Print the list of newly added lines
-# for line in new_lines:
-#     print(line)
-
-# print("")
-# def create_database_and_tables(instance_id, database_id):
-#     """Creates a database and tables for sample data."""
-#     from google.cloud.spanner_admin_database_v1.types import \
-#         spanner_database_admin
-
-#     spanner_client = spanner.Client()
-#     database_admin_api = spanner_client.database_admin_api
-
-#     request = spanner_database_admin.CreateDatabaseRequest(
-#         parent=database_admin_api.instance_path(spanner_client.project, instance_id),
-#         create_statement=f"CREATE DATABASE `{database_id}`",
-#         extra_statements=new_lines
-#     )
-
-#     operation = database_admin_api.create_database(request=request)
-
-#     print("Waiting for operation to complete...")
-#     database = operation.result(OPERATION_TIMEOUT_SECONDS)
-
-#     print(
-#         "Created database {} on instance {}".format(
-#             database.name,
-#             database_admin_api.instance_path(spanner_client.project, instance_id),
-#         )
-#     )
-#
-# create_database_and_tables(instance_id, database_id)
-
-#################################
-
-# import os
-# import subprocess
-
-# from google.cloud import spanner
 instance_id = "demo-instance"
 database_id = "demo-database"
 OPERATION_TIMEOUT_SECONDS = 240
@@ -131,7 +51,7 @@ def create_database(instance_id, database_id, new_lines):
     except Exception as e:
         print(f"An error occurred: {e}")
         raise e
-        
+
 print("Running python script...")
 
 print("\nCurrent directory:")
@@ -177,8 +97,6 @@ for line in new_lines:
     print(line)
 
 print("Starting the table creation...")
-
-
 
 # Call the create_database function
 create_database(instance_id, database_id, new_lines)
