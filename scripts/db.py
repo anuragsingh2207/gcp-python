254a66552c52b0b1cb470eef1a908611c343a51b
Anurag Kumar Singh <64636228+anuragsingh2207@users.noreply.github.com>
2024-07-18 08:16:28 +0530

    testing

--- scripts/db.py
+++ scripts/db.py
@@ -11,11 +11,7 @@ print("Listing files in current directory")
 result = subprocess.run(["ls", "-l"], capture_output=True, text=True)
 print(result.stdout)
 
-# command_string = "cd .. && ls -l "
-# result1 = subprocess.run(command_string, capture_output=True, text=True, shell=True)
-# print(result1.stdout)
-
-print("Changing directory to 'SQL' and staying there")
+print("Changing directory to 'sql' and staying there")
 os.chdir('../sql')
 
 print("Current directory after change:")
@@ -27,11 +23,11 @@ result = subprocess.run(["ls", "-l"], capture_output=True, text=True)
 print(result.stdout)
 
 
-# Perform git diff
-# result = subprocess.run(['git', 'diff', '--unified=0', 'HEAD', 'db.sql'], stdout=subprocess.PIPE)
-# diff_output = result.stdout.decode('utf-8')
+Perform git diff
+result = subprocess.run(['git', 'diff', '--unified=0', 'HEAD', 'db.sql'], stdout=subprocess.PIPE)
+diff_output = result.stdout.decode('utf-8')
 
-# print(diff_output)
+print(diff_output)
 
 # # Extract additions in the current uncommitted changes
 # new_lines = [line[1:] for line in diff_output.splitlines() if line.startswith('+') and not line.startswith('+++')]
