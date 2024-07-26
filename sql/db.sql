92b27785ef219749a591c48b665f79262da367c0
Anurag Kumar Singh <64636228+anuragsingh2207@users.noreply.github.com>
2024-07-26 23:23:32 +0530

    Real testing

--- sql/db.sql
+++ sql/db.sql
@@ -15,3 +15,21 @@ CREATE TABLE Albums1 (
     AlbumTitle   STRING(MAX)
 ) PRIMARY KEY (SingerId, AlbumId)
 ;
+
+CREATE TABLE Singers12 (
+    SingerId     INT64 NOT NULL,
+    FirstName    STRING(1024),
+    LastName     STRING(1024),
+    SingerInfo   BYTES(MAX),
+    FullName     STRING(2048) AS (
+        ARRAY_TO_STRING([FirstName, LastName], " ")
+    ) STORED
+) PRIMARY KEY (SingerId)
+;
+
+CREATE TABLE Albums12 (
+    SingerId     INT64 NOT NULL,
+    AlbumId      INT64 NOT NULL,
+    AlbumTitle   STRING(MAX)
+) PRIMARY KEY (SingerId, AlbumId)
+;
